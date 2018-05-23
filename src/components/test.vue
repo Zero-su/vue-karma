d9118a83c040201f9e1b4ad0e8bfce92e659d8e3
su.san <345469066@qq.com>
2018-05-23 16:13:23 +0800

    'first-commit'

---
+++ src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{data}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            data: '555'
+        }
+    }
+}
+</script>
