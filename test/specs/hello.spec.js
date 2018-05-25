aca298c1a5b4bdd64275ab3dc9a93036ecd2e130
Zero-su <345469066@qq.com>
2018-05-25 16:05:27 +0800

    commit

--- test/specs/hello.spec.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import hello from '@/components/hello.vue'
-
-describe('hello', () => {
-    it('hello world', () => {
-        var vm = new Vue(hello).$mount();
-        expect(vm.title).to.equal('hello world')
-    })
-})
